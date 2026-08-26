de1e8ecd23ae3898555dc5eabea9eb9d48548f31
PalzSoftware <s.suzuki2410@gmail.com>
2026-08-26 13:47:13 +0900

    Add v2 cross-search result types

---
+++ v2-web/src/types/search.ts
@@ -0,0 +1,19 @@
+export type SearchEntityType =
+  | "character"
+  | "move"
+  | "combo"
+  | "counter"
+  | "training"
+  | "player"
+  | "tournament"
+  | "video"
+  | "glossary";
+
+export type SearchResultItem = {
+  id: string;
+  type: SearchEntityType;
+  title: string;
+  subtitle?: string | null;
+  href: string;
+  matchedBy: "name" | "alias" | "content";
+};
